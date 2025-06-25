538ed82b425a434f658be31b59eb2bd5e3553c58
EscobarLucas73 <escobarlucas17@gmail.com>
2025-06-25 10:18:43 -0300

    subida de archivos

---
+++ GITHUB PROGRA/Modularizacion/funciones.c
@@ -0,0 +1,13 @@
+/*Implementaciond de funciones*/
+#include "funciones.h"
+int suma(int a, int b) {
+    return a + b;
+}
+int resta(int a, int b) {
+    return a - b;
+}
+int multiplicacion(int a, int b) {
+    return a * b;
+}
+
+// This code implements the functions declared in funciones.h.
